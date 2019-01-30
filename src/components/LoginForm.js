cbd4a27b3bb14818d3802fbbf7dbabf0895341aa
IMH51 <ianmartinharrison@outlook.com>
2019-01-30 22:26:31 +0000

    created controlled forms within react

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  updateState = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (this.state.username && this.state.password) this.props.onSubmit(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" value={this.state.userName} type="text" onChange={this.updateState} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" value={this.state.password} type="password" onChange={this.updateState} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
